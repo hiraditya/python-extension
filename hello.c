c6caf40f4e9ed2bf960a05b6d935ece1f5bbea2f
Aditya Kumar <aditya.k7@samsung.com>
2017-01-17 16:31:58 -0600

    executing command in c

--- hello.c
+++ hello.c
@@ -1,21 +1,83 @@
+// https://www.tutorialspoint.com/python/python_further_extensions.htm
+
 #include <Python.h>
 
 static PyObject* helloworld(PyObject* self)
 {
-    return Py_BuildValue("s", "Hello, Python test!!");
+  return Py_BuildValue("s", "Hello world");
+}
+
+static PyObject* helloworldname(PyObject* self)
+{
+  return Py_BuildValue("i", 100);
+}
+
+static PyObject *SpamError;
+
+static PyObject *
+spam_system(PyObject *self, PyObject *args)
+{
+  const char *command;
+  int sts;
+
+  if (!PyArg_ParseTuple(args, "s", &command))
+    return NULL;
+
+  sts = system(command);
+  if (sts < 0) {
+    PyErr_SetString(SpamError, "System command failed");
+    return NULL;
+  }
+  return PyLong_FromLong(sts);
 }
 
 static char helloworld_docs[] =
-    "helloworld( ): Any message you want to put here!!\n";
+  "helloworld( ): Prints hello world.\n";
+
+static char spam_docs[] =
+  "spam_system( ): Executes the command on shell.\n";
+
+/* Syntax of PyMethodDef
+struct PyMethodDef {
+  char *ml_name;
+  PyCFunction ml_meth;
+  int ml_flags;
+  char *ml_doc;
+};
+*/
 
 static PyMethodDef helloworld_funcs[] = {
-    {"helloworld", (PyCFunction)helloworld, 
-     METH_NOARGS, helloworld_docs},
-    {NULL}
+  { "helloworld",
+    (PyCFunction)helloworld,
+    METH_NOARGS,
+    helloworld_docs
+  },
+
+  { "helloworld_name", // Function name referred by python interpreter
+    (PyCFunction)helloworldname, // The C function
+    METH_NOARGS,
+    helloworld_docs
+  },
+
+  { "spam_system",
+    (PyCFunction)spam_system,
+    METH_VARARGS,
+    spam_docs
+  },
+
+  { NULL, NULL, 0, NULL } // Sentinel
 };
 
 void inithelloworld(void)
 {
-    Py_InitModule3("helloworld", helloworld_funcs,
-                   "Extension module example!");
+  PyObject *m;
+  m = Py_InitModule3("helloworld", helloworld_funcs,
+      "Extension module example!");
+
+  if (m == NULL)
+      return;
+  SpamError = PyErr_NewException("spam.error", NULL, NULL);
+  Py_INCREF(SpamError);
+  PyModule_AddObject(m, "error", SpamError);
 }
+
--- test_hello.py
+++ test_hello.py
@@ -2,3 +2,6 @@
 import helloworld
 
 print helloworld.helloworld()
+print helloworld.helloworld_name() + 100
+print helloworld.spam_system("ls -l")
+
